701588995311bbb26e8aff7264281376c423fccd
khoerul umam <khoerul.datalake@datalake.id>
2026-07-24 05:53:11 +0700

    fix error

--- list-models.ts
+++ list-models.ts
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 async function main() {
   const models = await ai.models.list();
   for await (const model of models) {
-    if (model.name.includes("flash")) {
+    if (model.name?.includes("flash")) {
       console.log(model.name);
     }
   }
